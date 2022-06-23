fd159eb1c7dbb0e31f93004470babbe6289605cc
idean3885 <idean3885@gmail.com>
2022-06-23 23:46:37 +0900

    init: 베이스 프로젝트 생성
    JUnit5 테스트 코드 작성
    롬복 설정

---
+++ src/test/java/com/dykim/base/v1/controller/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.dykim.base.v1.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void 헬로출력() throws Exception{
+        String hello = "hello!";
+
+        mvc.perform(get("/v1/hello/helloPrint"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
